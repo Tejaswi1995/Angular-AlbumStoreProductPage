3f6e78b417cfc830a114df10e37c903609753b9f
Tejaswi1995 <tejaswi.vadakapur@gmail.com>
2021-06-29 11:41:06 +0530

    all levels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import{ProductService} from './product.service';
+import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+
+const appRoutes: Routes = [{ path: 'products', component: ProductListComponent },
+{ path: 'product/:id', component: ProductPageComponent },{path:'',redirectTo:'products',pathMatch:'full'},];
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +27,8 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
